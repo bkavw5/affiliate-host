dff75e3f4fdb3db4dcb885d33f25f20d7137ec05
bkavw5 <electronicmusic911@gmail.com>
2025-07-29 15:15:35 +0700

    Add files via upload

---
+++ adsp.js
@@ -0,0 +1,11 @@
+// trinhedit.js
+document.write(`
+  <div id='vuukle-wrapper'></div>  
+  <div class='vuukle-affiliate-box mobile-only' style='margin:16px 0;'>
+    <a href='https://shorten.asia/PV7J8kXk' rel='nofollow' target='_blank'>
+      <img alt='Shopee Banner' src='https://img.pikbest.com/templates/20240717/latest-product-catalog-banners-decorate-shopee_10672816.jpg!w700wp' style='width:100%; border-radius:6px;'/>
+      <p style='font-size:13px;margin:6px 0 0;'>🛒 Ưu đãi độc quyền cho người dùng Shopee!</p>
+    </a>
+  </div>
+  <style>@media(min-width:768px){.mobile-only{display:none!important}}</style>
+`);
